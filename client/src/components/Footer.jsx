0d789753d85b6a6f5dd69e68393e2503af699272
Deeksha-Singh1 <singhdeeksha1511@gmail.com>
2023-06-16 18:06:34 +0530

    cards

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+      <footer class="py-3 my-4">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+      <li class="nav-item"><Link to="/" class="nav-link px-2 text-muted">Home</Link></li>
+      <li class="nav-item"><Link to="#" class="nav-link px-2 text-muted">Features</Link></li>
+      <li class="nav-item"><Link to="/cart" class="nav-link px-2 text-muted">Pricing</Link></li>
+      <li class="nav-item"><Link to="/about" class="nav-link px-2 text-muted">About</Link></li>
+    </ul>
+    <p class="text-center text-muted">© 2022 BiteBuddy, Inc</p>
+  </footer>
+    </div>
   )
 }
 
--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import Card from '../components/Card'
+import Carousal from '../components/Carousal'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
+
 
 const Home = () => {
   return (
     <div>
       <div><Navbar/></div>
-      <div>Body</div>
+      <div><Carousal/></div>
+      <div className='m-3' >
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+      </div>
       <div><Footer/></div>
     </div>
   )
