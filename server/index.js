88314d3a914831fcbd1ad6df19a52b6d52c7e31d
Deeksha-Singh1 <singhdeeksha1511@gmail.com>
2023-06-17 21:05:24 +0530

    mongoose

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const mongoose= require('mongoose');
 const app = express()
 const port = 5000
+const mongoURI = 'mongodb+srv://deeksha1511:Deeksha1511@cluster0.ei3093u.mongodb.net/';
 
+const mongoDb = mongoose.connect(mongoURI,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(()=>{
+  console.log("db connected successfully");
+}).catch((err)=>{
+  console.log(err);
+});
 
 
 app.get('/', (req,res)=>{
